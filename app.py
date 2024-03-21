caadd6d1adca7e205c5a05d9da78098a8b38f948
NST64 <anupam19g@gmail.com>
2024-03-21 17:11:20 +0530

    Coding completed

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+import cv2
+UPLOAD_FOLDER = 'Upload'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def processImages(filename, operation):
+    print(f"Operation is {operation} and filename is {filename}")
+    img = cv2.imread(f"Upload/{filename}")
+    if operation == "cgp":
+        imgprocessed = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        newFilename = f"static/{filename}"
+        cv2.imwrite(newFilename, imgprocessed)
+        return newFilename
+
+    elif operation == "cjpg":
+        newFilename = f"static/{filename.split('.')[0]}.jpg"
+        cv2.imwrite(newFilename, img)
+        return newFilename
+    
+    elif operation == "cwebp":
+        newFilename = f"static/{filename.split('.')[0]}.webp"
+        cv2.imwrite(newFilename, img)
+        return newFilename
+    
+    elif operation == "cpng":
+        newFilename = f"static/{filename.split('.')[0]}.png"
+        cv2.imwrite(newFilename, img)
+        return newFilename
+    
+    else:
+        print("Unsupported operation")
+    pass
+
 
 @app.route('/')
 def homepage():
@@ -13,6 +53,29 @@ def about():
 @app.route('/contactus')
 def contact():
     return render_template("contactus.html", name="Anupam")
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        operation = request.form.get("operation")
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            new = processImages(filename, operation)
+            flash(f"Image processed successfully and file is <a href='/{new}' target='_blank'>here</a>")
+            return render_template("home.html")
+    render_template("home.html", name="Anupam" )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
