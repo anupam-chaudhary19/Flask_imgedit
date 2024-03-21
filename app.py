9e123c63d7ccce0091ffc22e4676f4a9cecaf755
NST64 <anupam19g@gmail.com>
2024-03-21 14:19:43 +0530

    Image editig application

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def homepage():
     return render_template("home.html", name="Anupam")
 
+@app.route('/about')
+def about():
+    return render_template("about.html", name="Anupam")
+
+@app.route('/contactus')
+def contact():
+    return render_template("contactus.html", name="Anupam")
+
 if __name__ == '__main__':
     app.run(debug=True)
